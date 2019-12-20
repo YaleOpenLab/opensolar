9de824527705e63dd70d208a28718d732c2d7f7a
Varunram <varunramganesh@gmail.com>
2019-12-20 11:02:45 -0500

    add price to contract, make oracle return price only

--- oracle/oracle.go
+++ oracle/oracle.go
@@ -3,6 +3,5 @@ package oracle
 // MonthlyBill returns the power tariffs for a month charged by the utility companies
 func MonthlyBill() float64 {
 	priceOfElectricity := 0.2
-	averageConsumption := float64(600)
-	return priceOfElectricity * averageConsumption
+	return priceOfElectricity
 }
